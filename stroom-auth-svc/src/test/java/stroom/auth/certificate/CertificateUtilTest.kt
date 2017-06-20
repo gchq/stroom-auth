c071a282e858dd18572536139070c90e966a2073
jc064522 <jc064522@gmail.com>
2017-06-20 13:42:40 +0100

    Add test
    Moved from Stroom

---
+++ stroom-auth-svc/src/test/java/stroom/auth/certificate/CertificateUtilTest.kt
@@ -0,0 +1,22 @@
+package stroom.auth.certificate
+
+import org.junit.Assert
+import org.junit.Test
+import stroom.auth.svc.certificate.CertificateUtil
+import stroom.auth.svc.certificate.extractCNFromDN
+import java.util.regex.Pattern
+
+class TestCertificateUtil {
+    @Test
+    fun testSpaceInCN() {
+        val dn = "CN=John Smith (johnsmith), OU=ouCode1, OU=ouCode2, O=oValue, C=GB"
+
+        Assert.assertEquals("CN=John Smith (johnsmith),OU=ouCode1,OU=ouCode2,O=oValue,C=GB",
+                CertificateUtil.dnToRfc2253(dn))
+        Assert.assertEquals("John Smith (johnsmith)", extractCNFromDN(dn))
+        Assert.assertEquals("johnsmith", CertificateUtil.extractUserIdFromCN(extractCNFromDN(dn)))
+
+        val pattern = Pattern.compile("CN=[^ ]+ [^ ]+ \\(?([a-zA-Z0-9]+)\\)?")
+        Assert.assertEquals("johnsmith", CertificateUtil.extractUserIdFromDN(dn, pattern))
+    }
+}
