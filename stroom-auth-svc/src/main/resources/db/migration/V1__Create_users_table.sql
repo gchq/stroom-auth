2f673d4bbe3967345aff6e1e80cb9d0d1fe6b7b9
jc064522 <jc064522@gmail.com>
2017-06-20 13:42:23 +0100

    Add initial Flyway migrations

---
+++ stroom-auth-svc/src/main/resources/db/migration/V1__Create_users_table.sql
@@ -0,0 +1,16 @@
+-- Following Simon Holywell's style guide: http://www.sqlstyle.guide/
+CREATE TABLE users (
+  id 				    MEDIUMINT NOT NULL AUTO_INCREMENT,
+  name				    VARCHAR(255) NOT NULL,
+  is_login_expired      BOOLEAN NOT NULL,
+  last_login            TIMESTAMP NULL,
+  password_expiration   DATETIME NULL,
+  password_hash         VARCHAR(255) NOT NULL,
+  total_login_failures  INT DEFAULT 0,
+  created_on 			TIMESTAMP NULL,
+  created_by_user		VARCHAR(255) NULL,
+  updated_on 			TIMESTAMP NULL,
+  updated_by_user 		VARCHAR(255) NULL,
+  PRIMARY KEY           (id),
+  UNIQUE 			    (name)
+) ENGINE=InnoDB DEFAULT CHARSET latin1;
