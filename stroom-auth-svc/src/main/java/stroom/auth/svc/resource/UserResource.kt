23c876d79b2593436b867b3d94bbd7a4b8fadd6e
jc064522 <jc064522@gmail.com>
2017-06-20 15:39:47 +0100

    Add endpoints for getting users (WiP)

--- stroom-auth-svc/src/main/java/stroom/auth/svc/resource/UserResource.kt
+++ stroom-auth-svc/src/main/java/stroom/auth/svc/resource/UserResource.kt
@@ -3,14 +3,16 @@ package stroom.auth.svc.resource
 import com.codahale.metrics.annotation.Timed
 import io.dropwizard.auth.Auth
 import org.jooq.DSLContext
+import org.jooq.JSONFormat
 import org.slf4j.LoggerFactory
 import stroom.auth.svc.Config
 import stroom.auth.svc.security.User
 import stroom.db.auth.Tables
-import stroom.db.auth.tables.records.UsersRecord
+import stroom.db.auth.Tables.USERS
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
@@ -22,20 +24,80 @@ class UserResource(config: Config) {
     private val LOGGER = LoggerFactory.getLogger(AuthenticationResource::class.java)
 
     @GET
-    @Path("/") // TODO query param
+    @Path("/")
     @Timed
-    fun getUser(@Auth user: User, @Context database: DSLContext) : Response {
+    fun getAllUsers(
+            @Auth authenticatedUser: User,
+            @Context database: DSLContext,
+            @QueryParam("fromUsername") fromUsername: String,
+            @QueryParam("usersPerPage") usersPerPage: Int,
+            @QueryParam("orderBy") orderBy: String) : Response {
 
-        var foundUser: UsersRecord = database
-                .selectFrom(Tables.USERS)
-                .where(Tables.USERS.NAME.eq(user.name))
-                .single()
+        val orderByField = USERS.field(orderBy) ?:
+            return Response
+                .status(Response.Status.BAD_REQUEST)
+                .entity("Unknown orderBy field")
+                .build()
+        if(usersPerPage <=0 || usersPerPage >= 100) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity("usersPerPage must be between 0-100")
+                    .build()
+        }
+        if(database.select(USERS.ID).from(USERS).fetch().isEmpty()) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity("fromUsername does not refer to a user that exists in the database")
+                    .build()
+        }
 
+        val users = database
+                .select(USERS.ID,
+                        USERS.NAME,
+                        USERS.TOTAL_LOGIN_FAILURES,
+                        USERS.LAST_LOGIN,
+                        USERS.UPDATED_ON,
+                        USERS.UPDATED_BY_USER,
+                        USERS.CREATED_ON,
+                        USERS.CREATED_BY_USER)
+                .from(USERS)
+                .orderBy(USERS.NAME)
+                .seekAfter(fromUsername)
+                .limit(usersPerPage)
+                .fetch()
+                .formatJSON(JSONFormat().header(false).recordFormat(JSONFormat.RecordFormat.OBJECT))
+
+        return Response
+                .status(Response.Status.OK)
+                .entity(users)
+                .build()
+    }
+
+    @GET
+    @Path("/me")
+    @Timed
+    fun getCurrentUser(@Auth user: User, @Context database: DSLContext) : Response {
+
+        val foundUser = database
+                .select(
+                        USERS.ID,
+                        USERS.NAME,
+                        USERS.TOTAL_LOGIN_FAILURES,
+                        USERS.LAST_LOGIN,
+                        USERS.UPDATED_ON,
+                        USERS.UPDATED_BY_USER,
+                        USERS.CREATED_ON,
+                        USERS.CREATED_BY_USER)
+                .from(USERS)
+                .where(USERS.NAME.eq(user.name))
+                .fetch()
+                .formatJSON(JSONFormat().header(false).recordFormat(JSONFormat.RecordFormat.OBJECT))
 
         return Response
                 .status(Response.Status.OK)
                 .entity(foundUser)
                 .build()
     }
+
 
 }
