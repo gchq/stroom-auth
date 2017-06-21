86e1b05adbfc9a970b3c4b82563bdbf08247619e
jc064522 <jc064522@gmail.com>
2017-06-21 16:39:13 +0100

    Improve naming

--- stroom-auth-svc/src/integration-test/java/stroom/auth/service/resources/AuthenticationResource_IT.kt
+++ stroom-auth-svc/src/integration-test/java/stroom/auth/service/resources/AuthenticationResource_IT.kt
@@ -1,7 +1,6 @@
 package stroom.auth.service.resources
 
 import com.github.kittinunf.fuel.httpPost
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.slf4j.LoggerFactory
 
@@ -23,65 +22,61 @@ class AuthenticationResource_IT : Base_IT() {
     }
 
     @Test
-    fun bad_credentials_1() {
-        val (request, response) = LOGIN_URL
+    fun incorrect_credentials_1() {
+        val (_, response) = LOGIN_URL
                 .httpPost()
                 .body("{\"username\" : \"BAD\", \"password\" : \"admin\"}")
-                .header(mapOf("Content-Type" to "application/json"))
+                .header(JSON_CONTENT_TYPE)
                 .responseString()
-        assertThat(response.httpStatusCode).isEqualTo(401)
+        assertUnauthorised(response)
     }
 
     @Test
-    fun bad_credentials_2() {
-        val (request, response) = LOGIN_URL
+    fun incorrect_credentials_2() {
+        val (_, response) = LOGIN_URL
                 .httpPost()
                 .body("{\"username\" : \"admin\", \"password\" : \"BAD\"}")
-                .header(mapOf("Content-Type" to "application/json"))
+                .header(JSON_CONTENT_TYPE)
                 .responseString()
-        assertThat(response.httpStatusCode).isEqualTo(401)
+        assertUnauthorised(response)
     }
 
     @Test
-    fun bad_credentials_3() {
-        val (request, response) = LOGIN_URL
+    fun incorrect_credentials_3() {
+        val (_, response) = LOGIN_URL
                 .httpPost()
                 .body("{\"username\" : \"BAD\", \"password\" : \"BAD\"}")
-                .header(mapOf("Content-Type" to "application/json"))
+                .header(JSON_CONTENT_TYPE)
                 .responseString()
-        assertThat(response.httpStatusCode).isEqualTo(401)
+        assertUnauthorised(response)
     }
 
     @Test
-    fun bad_credentials_4() {
-        val (request, response) = LOGIN_URL
+    fun missing_credentials_1() {
+        val (_, response) = LOGIN_URL
                 .httpPost()
                 .body("{\"username\" : \"BAD\"}")
-                .header(mapOf("Content-Type" to "application/json"))
+                .header(JSON_CONTENT_TYPE)
                 .responseString()
-        assertThat(response.httpStatusCode).isEqualTo(400)
+        assertBadRequest(response)
     }
 
     @Test
-    fun bad_credentials_5() {
-        val (request, response) = LOGIN_URL
+    fun missing_credentials_2() {
+        val (_, response) = LOGIN_URL
                 .httpPost()
                 .body("{\"password\" : \"BAD\"}")
-                .header(mapOf("Content-Type" to "application/json"))
+                .header(JSON_CONTENT_TYPE)
                 .responseString()
-        assertThat(response.httpStatusCode).isEqualTo(400)
+        assertBadRequest(response)
     }
 
     @Test
-    fun bad_credentials_6() {
-        val (request, response) = LOGIN_URL
+    fun missing_credentials_3() {
+        val (_, response) = LOGIN_URL
                 .httpPost()
-                .header(mapOf("Content-Type" to "application/json"))
+                .header(JSON_CONTENT_TYPE)
                 .responseString()
-        assertThat(response.httpStatusCode).isEqualTo(400)
+        assertBadRequest(response)
     }
-
-
-
-
 }
