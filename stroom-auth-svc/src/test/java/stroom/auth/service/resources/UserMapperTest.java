8e69c2b67fc92b90e10eb91192114974c9f4d76f
jc064522 <jc064522@gmail.com>
2017-06-30 15:48:43 +0100

    Kotlin -> Java

---
+++ stroom-auth-svc/src/test/java/stroom/auth/service/resources/UserMapperTest.java
@@ -0,0 +1,71 @@
+package stroom.auth.service.resources;
+
+import org.junit.Test;
+import stroom.db.auth.tables.records.UsersRecord;
+
+import java.sql.Timestamp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class UserMapperTest {
+    @Test
+    public final void testMappings() {
+        UsersRecord usersRecord = new UsersRecord();
+        usersRecord.setId(Integer.valueOf(1));
+        usersRecord.setEmail("email");
+        usersRecord.setPasswordHash("hash");
+        usersRecord.setState("enabled");
+        usersRecord.setFirstName("first name");
+        usersRecord.setLastName("last name");
+        usersRecord.setComments("comments");
+        usersRecord.setLoginFailures(Integer.valueOf(2));
+        usersRecord.setLoginCount(Integer.valueOf(4));
+        usersRecord.setLastLogin(new Timestamp(System.currentTimeMillis()));
+        usersRecord.setUpdatedOn(new Timestamp(System.currentTimeMillis()));
+        usersRecord.setUpdatedByUser("updating user");
+        usersRecord.setCreatedOn(new Timestamp(System.currentTimeMillis()));
+        usersRecord.setCreatedByUser("creating user");
+
+        User user = new User();
+        user.setId(Integer.valueOf(2));
+        user.setEmail("new email");
+        user.setPassword_hash("new hash");
+        user.setState("disabled");
+        user.setFirst_name("new first name");
+        user.setLast_name("new last name");
+        user.setComments("new comments");
+        user.setLogin_failures(Integer.valueOf(3));
+        user.setLogin_count(Integer.valueOf(5));
+        user.setLast_login("2017-01-01T00:00:00.000Z");
+        user.setUpdated_on("2017-01-02T00:00:00.000Z");
+        user.setUpdated_by_user("New updating user");
+        user.setCreated_on("2017-01-03T00:00:00.000Z");
+        user.setCreated_by_user("New creating user");
+
+        UsersRecord updatedRecord = UserMapper.updateUserRecordWithUser(user, usersRecord);
+        assertThat(updatedRecord.getId()).isEqualTo(2);
+        assertThat(updatedRecord.getEmail()).isEqualTo("new email");
+        assertThat(updatedRecord.getPasswordHash()).isEqualTo("new hash");
+        assertThat(updatedRecord.getState()).isEqualTo("disabled");
+        assertThat(updatedRecord.getFirstName()).isEqualTo("new first name");
+        assertThat(updatedRecord.getLastName()).isEqualTo("new last name");
+        assertThat(updatedRecord.getComments()).isEqualTo("new comments");
+        assertThat(updatedRecord.getLoginFailures()).isEqualTo(3);
+        assertThat(updatedRecord.getLoginCount()).isEqualTo(5);
+        assertThat(updatedRecord.getLastLogin()).isEqualTo(UserMapper.convertISO8601ToTimestamp("2017-01-01T00:00:00.000Z"));
+        assertThat(updatedRecord.getUpdatedOn()).isEqualTo(UserMapper.convertISO8601ToTimestamp("2017-01-02T00:00:00.000Z"));
+        assertThat(updatedRecord.getUpdatedByUser()).isEqualTo("New updating user");
+        assertThat(updatedRecord.getCreatedOn()).isEqualTo(UserMapper.convertISO8601ToTimestamp("2017-01-03T00:00:00.000Z"));
+        assertThat(updatedRecord.getCreatedByUser()).isEqualTo("New creating user");
+    }
+
+    @Test
+    public final void updateReminder() {
+        // This test will fail if you add a property to the Users class.
+        // To make it pass you'll need to updated the count, below.
+        // It exists to remind you to add a mapping in UserMapper.
+        // The count is one more than the number in UserRecord, because it has a 'password' property.
+        int currentNumberOfPropertiesOnUser = 15;
+        assertThat(User.class.getDeclaredFields().length).isEqualTo(currentNumberOfPropertiesOnUser);
+    }
+}
---
+++ stroom-auth-svc/src/test/java/stroom/auth/service/security/CertificateUtilTest.java
@@ -0,0 +1,17 @@
+package stroom.auth.service.security;
+
+import java.util.regex.Pattern;
+import org.junit.Assert;
+import org.junit.Test;
+
+public final class CertificateUtilTest {
+    @Test
+    public final void testSpaceInCN() {
+        String dn = "CN=John Smith (johnsmith), OU=ouCode1, OU=ouCode2, O=oValue, C=GB";
+        Assert.assertEquals("CN=John Smith (johnsmith),OU=ouCode1,OU=ouCode2,O=oValue,C=GB", CertificateUtil.dnToRfc2253(dn));
+        Assert.assertEquals("John Smith (johnsmith)", CertificateUtil.extractCNFromDN(dn));
+        Assert.assertEquals("johnsmith", CertificateUtil.extractUserIdFromCN(CertificateUtil.extractCNFromDN(dn)));
+        Pattern pattern = Pattern.compile("CN=[^ ]+ [^ ]+ \\(?([a-zA-Z0-9]+)\\)?");
+        Assert.assertEquals("johnsmith", CertificateUtil.extractUserIdFromDN(dn, pattern));
+    }
+}
