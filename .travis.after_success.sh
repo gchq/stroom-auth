2a7ea16e6475c37182c27a269abbb6222740999d
jc064522 <jc064522@gmail.com>
2017-08-25 15:52:34 +0100

    :construction_worker: :bug: Fixing another obvious syntax error

--- .travis.after_success.sh
+++ .travis.after_success.sh
@@ -8,10 +8,10 @@ AUTH_SERVICE_TAG="gchq/stroom-auth-service:${TRAVIS_TAG}"
 echo "Building stroom-auth-service with tag ${AUTH_SERVICE_TAG}"
 docker build --tag=${AUTH_SERVICE_TAG} stroom-auth-svc/.
 echo "Pushing ${AUTH_SERVICE_TAG}"
-docker push AUTH_SERVICE_TAG
+docker push ${AUTH_SERVICE_TAG}
 
 AUTH_UI_TAG="gchq/stroom-auth-ui:${TRAVIS_TAG}"
 echo "Building stroom-auth-ui with tag ${AUTH_UI_TAG}"
 docker build --tag=${AUTH_UI_TAG} stroom-auth-svc/.
 echo "Pushing ${AUTH_UI_IMAGE}"
-docker push AUTH_UI_TAG
+docker push ${AUTH_UI_TAG}
