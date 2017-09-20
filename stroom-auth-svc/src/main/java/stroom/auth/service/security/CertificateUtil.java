c6ee6551bceca4b8999ba1d0cfefaa20a4938a18
jc064522 <jc064522@gmail.com>
2017-09-20 08:11:22 +0100

    Add/update copyright

--- stroom-auth-svc/src/main/java/stroom/auth/service/security/CertificateUtil.java
+++ stroom-auth-svc/src/main/java/stroom/auth/service/security/CertificateUtil.java
@@ -1,6 +1,5 @@
-package stroom.auth.service.security;
 /*
- * Copyright 2016 Crown Copyright
+ * Copyright 2017 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +13,8 @@ package stroom.auth.service.security;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package stroom.auth.service.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
