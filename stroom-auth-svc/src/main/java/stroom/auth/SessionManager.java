e3508d1807a3b6708362c0b3ad65490b31bb61cc
jc064522 <jc064522@gmail.com>
2017-11-10 15:24:55 +0000

    Remove unused code

--- stroom-auth-svc/src/main/java/stroom/auth/SessionManager.java
+++ stroom-auth-svc/src/main/java/stroom/auth/SessionManager.java
@@ -20,9 +20,7 @@ package stroom.auth;
 
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientResponse;
-import stroom.auth.config.Config;
 
-import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -38,22 +36,8 @@ public class SessionManager {
     private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(SessionManager.class);
 
     Map<String, Session> sessions = new HashMap<>();
-    private Config config;
-    private TokenBuilderFactory tokenBuilderFactory;
 
     private Client logoutClient = ClientBuilder.newClient(new ClientConfig().register(ClientResponse.class));
-
-
-    @Inject
-    public SessionManager(Config config, TokenBuilderFactory tokenBuilderFactory) {
-        this.config = config;
-        this.tokenBuilderFactory = tokenBuilderFactory;
-    }
-
-    public boolean isAuthenticated(String sessionId) {
-        Session session = getOrCreate(sessionId);
-        return session != null && session.isAuthenticated();
-    }
 
     public static String createAccessCode() {
         SecureRandom secureRandom = new SecureRandom();
@@ -61,16 +45,6 @@ public class SessionManager {
         secureRandom.nextBytes(bytes);
         String accessCode = Base64.getUrlEncoder().encodeToString(bytes);
         return accessCode;
-    }
-
-    public Session getOrCreate(String sessionId) {
-        Session session = sessions.get(sessionId);
-        if (session == null) {
-            session = new Session();
-            session.setSessionId(sessionId);
-            sessions.put(sessionId, session);
-        }
-        return session;
     }
 
     public Optional<Session> get(String id) {
