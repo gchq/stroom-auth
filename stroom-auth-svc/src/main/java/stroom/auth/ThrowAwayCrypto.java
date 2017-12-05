8f5a3da9ca39c849a3f880a18006757e1519b482
jc064522 <jc064522@gmail.com>
2017-12-05 15:22:45 +0000

    Cleaning up obsolete file

--- stroom-auth-svc/src/main/java/stroom/auth/ThrowAwayCrypto.java
+++
@@ -1,8 +0,0 @@
-package stroom.auth;
-
-    import org.slf4j.Logger;
-    import org.slf4j.LoggerFactory;
-
-public class ThrowAwayCrypto {
-    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(ThrowAwayCrypto.class);
-}
