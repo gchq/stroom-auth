318d2809e5de430f9baa29af9e793fa4180d88c7
jc064522 <jc064522@gmail.com>
2018-01-03 14:12:24 +0000

    Fix CI

--- travis.before_script.sh
+++ travis.before_script.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+# Any attempt to check this will fail because the caller (possibly a guest container)
+# won't be able to talk back to the host
 export ENABLE_TOKEN_REVOCATION_CHECK=false
 
 # The Authorisation Service, which runs in the Stroom container, needs to talk back to the auth-service,
 # which runs on the host. So we need to add a name to the host so it points back to us.
 export STROOM_RESOURCES_ADVERTISED_HOST=`ip route get 1 | awk '{print $NF;exit}'`
-echo $STROOM_RESOURCES_ADVERTISED_HOST
 
 # Increase the size of the heap
-echo $JAVA_OPTS
 export JAVA_OPTS=-Xmx1024m
+
+echo "127.0.0.1 kafka" >> /etc/hosts
+echo "127.0.0.1 hbase" >> /etc/hosts
+echo "127.0.0.1 auth-service" >> /etc/hosts
 
 # Get stroom-resources and start the DBs
 mkdir -p ../git_work
