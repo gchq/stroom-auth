4c44369e072bcf11b97cac71d4a727f1924273a2
jc064522 <jc064522@gmail.com>
2018-01-08 11:49:28 +0000

    Update CI with recent script from Stroom

--- travis.before_script.sh
+++ travis.before_script.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
-# Any attempt to check this will fail because the caller (possibly a guest container)
-# won't be able to talk back to the host
-export ENABLE_TOKEN_REVOCATION_CHECK=false
 
-# The Authorisation Service, which runs in the Stroom container, needs to talk back to the auth-service,
-# which runs on the host. So we need to add a name to the host so it points back to us.
-export STROOM_RESOURCES_ADVERTISED_HOST=`ip route get 1 | awk '{print $NF;exit}'`
+#exit script on any error
+set -e
 
-# Increase the size of the heap
-export JAVA_OPTS=-Xmx1024m
+#Shell Colour constants for use in 'echo -e'
+#e.g.  echo -e "My message ${GREEN}with just this text in green${NC}"
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+NC='\033[0m' # No Colour
 
 sudo bash -c "echo '127.0.0.1 kafka' >> /etc/hosts"
 sudo bash -c "echo '127.0.0.1 hbase' >> /etc/hosts"
-sudo bash -c "echo '127.0.0.1 auth-service' >> /etc/hosts"
+sudo bash -c "echo '127.0.0.1 stroom-auth-service' >> /etc/hosts"
 
-# Get stroom-resources and start the DBs
-mkdir -p ../git_work
-pushd ../git_work
-git clone https://github.com/gchq/stroom-resources.git
-pushd stroom-resources/bin
-git checkout $RESOURCES_BRANCH
-# Start all the services we need to run the integration tests in stroom
-nohup ./bounceIt.sh up -e -y --build kafka zookeeper stroom stroom-db stroom-stats-db stroom-auth-service-db &
-popd
-popd
+echo -e "TRAVIS_EVENT_TYPE:   [${GREEN}${TRAVIS_EVENT_TYPE}${NC}]"
+
+if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+    echo "Cron build so don't set up gradle plugins or docker containers"
+
+else
+    echo -e "JAVA_OPTS: [${GREEN}$JAVA_OPTS${NC}]"
+    # Increase the size of the heap
+    export JAVA_OPTS=-Xmx1024m
+
+    echo "Clone build and publish our urlDependencies plugin"
+    mkdir -p ../git_work
+    pushd ../git_work
+    git clone https://github.com/gchq/urlDependencies-plugin.git
+    pushd urlDependencies-plugin
+    ./gradlew clean build publishToMavenLocal
+    popd
+
+    echo "Clone our stroom-resources repo"
+    git clone https://github.com/gchq/stroom-resources.git
+    pushd stroom-resources/bin
+    git checkout $STROOM_RESOURCES_BRANCH
+
+    echo "Start all the services we need to run the integration tests in stroom"
+    ./bounceIt.sh 'up -d --build' -e -y -x kafka zookeeper stroom stroom-db stroom-stats-db stroom-auth-service-db
+    popd
+    popd
+fi
+
+exit 0
