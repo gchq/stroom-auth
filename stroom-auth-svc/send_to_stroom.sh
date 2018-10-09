0b18e821eb0080fd3d26820dc3dd877043c450d1
jc064522 <jc064522@gmail.com>
2018-10-09 10:23:51 +0100

    Refactor

--- stroom-auth-svc/send_to_stroom.sh
+++ stroom-auth-svc/send_to_stroom.sh
@@ -6,6 +6,7 @@
 # 'argbash send_to_stroom_args.m4 -o send_to_stroom_args.sh'
 source send_to_stroom_args.sh
 
+# Create references to the args
 readonly LOG_DIR=$_arg_log_dir
 readonly FEED=$_arg_feed
 readonly SYSTEM=$_arg_system
@@ -15,12 +16,12 @@ readonly SECURE=$_arg_secure
 readonly MAX_SLEEP=$_arg_max_sleep
 readonly DELETE_AFTER_SENDING=$_arg_delete_after_sending
 
+## Configure other constants
 readonly LCK_FILE=${LOG_DIR}/`basename $0`.lck
 readonly RANDOM=`echo $RANDOM`
 readonly MOD=`expr $MAX_SLEEP + 1`
 readonly SLEEP=`expr $RANDOM % $MOD`
 readonly THIS_PID=`echo $$`
-
 
 setup_echo_colours() {
     # Exit the script on any error
@@ -36,17 +37,16 @@ setup_echo_colours() {
     NC='\033[0m' # No Color
 }
 
-
-
-send_files() {
+configure_curl() {
     if [ "${SECURE}" = "false" ]; then
             CURL_OPTS="-k "
             echo -e "${YELLOW}Warn:${NC} Running in insecure mode where we do not check SSL certificates. CURL_OPTS=${CURL_OPTS}"
     else
             CURL_OPTS=""
     fi
+}
 
-
+get_lock() {
     if [ -f "${LCK_FILE}" ]; then
             MYPID=`head -n 1 "${LCK_FILE}"`
             TEST_RUNNING=`ps -p ${MYPID} | grep ${MYPID}`
@@ -62,42 +62,46 @@ send_files() {
             echo -e "${GREEN}Info:${NC} Obtained locked for ${THIS_PID}, processing directory ${LOG_DIR}"
             echo "${THIS_PID}" > "${LCK_FILE}"
     fi
+}
 
+send_files() {
     echo -e "${GREEN}Info:${NC} Will sleep for ${SLEEP}s to help balance network traffic"
     sleep ${SLEEP}
 
     for FILE in `find ${LOG_DIR} -name '*.log'`
     do
-            echo -e "\n${GREEN}Info:${NC} Processing ${FILE}"
-            RESPONSE_HTTP=`curl ${CURL_OPTS} --write-out "RESPONSE_CODE=%{http_code}" --data-binary @${FILE} "${STROOM_URL}" -H "Feed:${FEED}" -H "System:${SYSTEM}" -H "Environment:${ENVIRONMENT}" 2>&1`
-            RESPONSE_LINE=`echo ${RESPONSE_HTTP} | head -1`
-            RESPONSE_MSG=`echo ${RESPONSE_HTTP} | grep -o -e RESPONSE_CODE=.*$`
-            RESPONSE_CODE=`echo ${RESPONSE_MSG} | cut -f2 -d '='`
-            if [ "${RESPONSE_CODE}" != "200" ]
-            then
-                    echo -e "${RED}Error:${NC} Unable to send file ${FILE}, error was ${RESPONSE_LINE}"
-            else
-                    echo -e "${GREEN}Info:${NC} Sent file ${FILE}, response code was ${RESPONSE_CODE}"
-
-                    if [ "${DELETE_AFTER_SENDING}" = "on" ]; then
-                        echo -e "${YELLOW}Warn:${NC} Deleting successfully sent file ${FILE}"
-                        rm ${FILE}
-                    fi
-            fi
+        send_file
     done
 
     rm ${LCK_FILE}
 }
-#
-#send_file() {
-#    echo -e "send file"
-#}
+
+send_file() {
+    echo -e "\n${GREEN}Info:${NC} Processing ${FILE}"
+    RESPONSE_HTTP=`curl ${CURL_OPTS} --write-out "RESPONSE_CODE=%{http_code}" --data-binary @${FILE} "${STROOM_URL}" -H "Feed:${FEED}" -H "System:${SYSTEM}" -H "Environment:${ENVIRONMENT}" 2>&1`
+    RESPONSE_LINE=`echo ${RESPONSE_HTTP} | head -1`
+    RESPONSE_MSG=`echo ${RESPONSE_HTTP} | grep -o -e RESPONSE_CODE=.*$`
+    RESPONSE_CODE=`echo ${RESPONSE_MSG} | cut -f2 -d '='`
+    if [ "${RESPONSE_CODE}" != "200" ]
+    then
+            echo -e "${RED}Error:${NC} Unable to send file ${FILE}, error was ${RESPONSE_LINE}"
+    else
+            echo -e "${GREEN}Info:${NC} Sent file ${FILE}, response code was ${RESPONSE_CODE}"
+
+            if [ "${DELETE_AFTER_SENDING}" = "on" ]; then
+                echo -e "${YELLOW}Warn:${NC} Deleting successfully sent file ${FILE}"
+                rm ${FILE}
+            fi
+    fi
+}
 
 main() {
     setup_echo_colours
     echo -e "${GREEN}Welcome to the send_to_stroom.sh script${NC}"
     echo -e "This script to send log files to Stroom."
 
+    configure_curl
+    get_lock
     send_files
 }
 
