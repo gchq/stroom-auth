2dc050b4128618b430ec83e9b270245efcab0fda
jc064522 <jc064522@gmail.com>
2018-10-12 10:30:27 +0100

    Fix var reference

--- docker_lib.sh
+++ docker_lib.sh
@@ -2,6 +2,10 @@
 #
 # Re-usable functions for building docker images
 
+readonly SERVICE_SOURCE_DIR="stroom-auth-svc"
+readonly SERVICE_DOCKER_DIR="${SERVICE_SOURCE_DIR}/docker"
+readonly SERVICE_BUILD_DIR="${SERVICE_DOCKER_DIR}/build"
+
 build_ui(){
     cd stroom-auth-ui/docker
     ./build.sh $TAG
@@ -9,23 +13,23 @@ build_ui(){
 }
 
 prep_service_build() {
-    local -r SOURCE_DIR="stroom-auth-svc"
-    local -r DOCKER_DIR="${SOURCE_DIR}/docker"
-    local -r BUILD_DIR="${DOCKER_DIR}/build"
+
     # Exclude tests because we want this to be fast. I guess you'd better test the build before releasing.
     ./gradlew clean build shadowJar -x test -x integrationTest
     pushd stroom-auth-svc
     ./config.yml.sh
     popd
-    mkdir -p ${BUILD_DIR}
-    cp -f ${SOURCE_DIR}/config.yml ${BUILD_DIR}/config.yml
-    cp -f ${SOURCE_DIR}/build/libs/stroom-auth-service-all.jar ${BUILD_DIR}/stroom-auth-service-all.jar
-    cp -f ${SOURCE_DIR}/send_to_stroom* ${BUILD_DIR}
+    mkdir -p ${SERVICE_BUILD_DIR}
+    cp -f ${SERVICE_SOURCE_DIR}/config.yml ${SERVICE_BUILD_DIR}/config.yml
+    cp -f ${SERVICE_SOURCE_DIR}/build/libs/stroom-auth-service-all.jar ${SERVICE_BUILD_DIR}/stroom-auth-service-all.jar
+    cp -f ${SERVICE_SOURCE_DIR}/send_to_stroom* ${SERVICE_BUILD_DIR}
 }
 
 build_service() {
     prep_service_build
-    docker build --tag gchq/stroom-auth-service:$TAG ${DOCKER_DIR}/.
+    echo "--${TAG}--"
+    echo "--${SERVICE_DOCKER_DIR}--"
+    docker build --tag gchq/stroom-auth-service:$TAG ${SERVICE_DOCKER_DIR}/.
 }
 
 push_ui() {
