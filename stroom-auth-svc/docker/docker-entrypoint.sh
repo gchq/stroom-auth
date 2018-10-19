ae3eba0bcd18d64db56c94b30b18be62b62d171b
at055612 <at055612@gmail.com>
2018-10-19 16:10:01 +0100

    Tidy up entrypoint file

--- stroom-auth-svc/docker/docker-entrypoint.sh
+++ stroom-auth-svc/docker/docker-entrypoint.sh
@@ -3,10 +3,9 @@ set -e
 
 # Re-set permission to the `auth` user if current user is root
 # This avoids permission denied if the data volume is mounted by root
-#if [ "$1" = 'auth' -a "$(id -u)" = '0' ]; then
 if [ "$(id -u)" = '0' ]; then
     chown -R auth:auth .
-    #su-exec is the alpine equivalent of gosu
+
     #runs all args as user auth, rather than as root
     exec gosu auth "$@"
 fi
