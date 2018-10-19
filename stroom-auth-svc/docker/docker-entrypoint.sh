31f499c2a302ffcda804dce72cffdcf46f698df9
at055612 <at055612@gmail.com>
2018-10-19 14:11:16 +0100

    Fix entrypoint/cmd and chown group as well

--- stroom-auth-svc/docker/docker-entrypoint.sh
+++ stroom-auth-svc/docker/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # This avoids permission denied if the data volume is mounted by root
 #if [ "$1" = 'auth' -a "$(id -u)" = '0' ]; then
 if [ "$(id -u)" = '0' ]; then
-    chown -R auth .
+    chown -R auth:auth .
     #su-exec is the alpine equivalent of gosu
     #runs all args as user auth, rather than as root
     exec gosu auth "$@"
