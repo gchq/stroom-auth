5bffce1dfe25e9faa35619d8db22d45ff8b92e3c
at055612 <at055612@gmail.com>
2018-10-19 16:10:24 +0100

    Add comments to run.sh

--- stroom-auth-svc/docker/run.sh
+++ stroom-auth-svc/docker/run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Build the crontab file to send logs to stroom
 ./create_crontab.sh &
+
+# Start the cron daemon
 cron &
+
+# Start the app
 java -jar stroom-auth-service-all.jar server config.yml
