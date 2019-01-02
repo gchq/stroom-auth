cae60e8c5a8865162a5ac8fd47a099b86361f958
jc064522 <jc064522@gmail.com>
2019-01-02 09:30:05 +0000

    Improve script help

--- stroom-persistence/src/scripts/transform_user_extract.py
+++ stroom-persistence/src/scripts/transform_user_extract.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
-"""Transforms an SQL extract of pre 6.0 users to 6.0 users, ready for import
+"""
+Transforms an SQL extract of pre-6.0 Stroom users to 6.0 users, ready for import to the stroom-auth database. If you're upgrading to Stroom 6.0 then this is a necessary step.
+
+This scripts expects the input TSV to have the following colums: NAME, STAT. 
+
+You could use something like the following SQL to dump the users from Stroom into a TSV:
+    echo 'SELECT NAME, STAT FROM USR' | mysql -h"192.168.1.9" -P"3307" -u"stroomuser" -p"stroompassword1" stroom > user_extract.tsv
+
+If you have the right permissions you could also get the extract like this by executing the following in mysql:
+    SELECT NAME, STAT INTO OUTFILE './user_extract.tsv' FROM USR;
 
 Usage: 
 transform_user_extract.py <input_tsv> <output_sql>
@@ -72,8 +81,6 @@ def transform(input_tsv, output_sql):
             print "Unable to process {0} user(s).".format(total - processed)
 
 def main():
-    print(docopt(__doc__, version='v1.0'))
-
     arguments = docopt(__doc__, version='v1.0')
     input_tsv = arguments["<input_tsv>"]
     output_sql = arguments["<output_sql>"]
