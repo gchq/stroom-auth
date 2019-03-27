5d22fdbd5ac6456e3d59f8c850cda1b132ea2b90
jc064522 <jc064522@gmail.com>
2019-03-27 11:30:01 +0000

    Storybooking

--- stroom-auth-ui_ts/src/components/Loader/Loader.stories.tsx
+++ stroom-auth-ui_ts/src/components/Loader/Loader.stories.tsx
@@ -1,14 +1,7 @@
-// import * as React from "react";
-// import { storiesOf } from "@storybook/react";
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
 
-// import { addThemedStories } from "../../lib/themedStoryGenerator";
-// import Loader from "./Loader";
-// import StroomDecorator from "../../testing/storybook/StroomDecorator";
+import Loader from "./Loader";
 
-// import "../../styles/main.css";
-
-// const stories = storiesOf("General Purpose/Loader", module).addDecorator(
-//   StroomDecorator
-// );
-
-// addThemedStories(stories, <Loader message="Stuff is loading" />);
+storiesOf("General Purpose/Loader", module)
+    .add("Basic", () => <Loader message="Stuff is loading" />);
